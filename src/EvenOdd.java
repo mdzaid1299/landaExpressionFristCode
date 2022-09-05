3384562be314193dc1bde65173e793288a497a8b
HP <mdzaid1299@gmail.com>
2022-09-05 13:51:41 +0530

    initial commit

--- src/EvenOdd.java
+++ src/EvenOdd.java
@@ -6,7 +6,7 @@ public interface EvenOdd{
 
 }
 
-class OddI8mpl {
+class OddImpl {
     public static void main(String[] args) {
         EvenOdd o1 = number -> (number % 2 != 0);
         System.out.println("num " + o1.isOdd(22));
